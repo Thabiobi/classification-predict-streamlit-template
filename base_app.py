cf6bf99273461e2a02da6236c0a11b4d45fee9a2
PTStace <68692872+PTStace@users.noreply.github.com>
2020-11-16 00:07:37 +0200

    eda hashtags pics

--- base_app.py
+++ base_app.py
@@ -27,6 +27,17 @@ import joblib,os
 
 # Data dependencies
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+from wordcloud import WordCloud
+from nltk.tokenize import sent_tokenize, word_tokenize
+from sklearn.metrics import accuracy_score
+
+# for plots
+import plotly.graph_objects as go
+from PIL import Image 
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
@@ -35,20 +46,81 @@ tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl f
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
 
+# cleaning text fucntion
+def clean_tweets(message):
+	""" This function removes punctions and url's from the message"""
+    
+	#change all words into lower case
+	message = message.lower()
+    
+	#replace website links
+	url = r'http[s]?://(?:[A-Za-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9A-Fa-f][0-9A-Fa-f]))+'
+	web = 'url-web'
+	message = re.sub(url, web, message)
+    
+	#removing puntuation and digits
+	message  = "".join([char for char in message if char not in string.punctuation])
+	message = re.sub('[0-9]+', '', message)
+    
+	#removing stopwords
+	nltk_stopword = nltk.corpus.stopwords.words('english')
+	message = ' '.join([item for item in message.split() if item not in nltk_stopword])
+    
+	return message
+
+
+# more text cleaning
+def cleaning (text):
+	"""this function lemmatizes the message"""
+    
+	text = re.sub(r'[^\w\s]','',text, re.UNICODE)
+	text = text.lower()
+
+	lemmatizer = WordNetLemmatizer()
+	text = [lemmatizer.lemmatize(token) for token in text.split(" ")]
+	text = [lemmatizer.lemmatize(token, "v") for token in text]
+
+	text = " ".join(text)
+	text = re.sub('ãââ', '', text)
+    
+	return text
+
+
+# sentiment prediction
+def statement(sentiment):
+	"""" """
+	# 'anti' text statement
+	if sentiment == -1:
+		st.success("tweet does not believe in man-made climate change")
+	# 'neutral' text statement
+	if sentiment == 0:
+		st.success("tweet neither supports nor refutes the belief of man-made climate change")
+	# 'pro' text sentiment
+	if sentiment == 1:
+		st.success("tweet upports the belief of man-made climate change")
+	# 'news' text statement
+	if sentiment == 2:
+		st.success("tweet links to factual news about climate change")
+	return 	
+
+
 # The main function where we will build the actual app
 def main():
 	"""Tweet Classifier App with Streamlit """
 
-	# Creates a main title and subheader on your page -
-	# these are static across all pages
-	st.title("Tweet Classifer")
-	st.subheader("Team 6 is awesome)
-	
-
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
-	selection = st.sidebar.selectbox("Choose Option", options)
+	options = ["Home","Information", "EDA", "Prediction", "About us"]
+	st.sidebar.subheader("Navigation")
+	selection = st.sidebar.selectbox("select a page", options)
+
+	# Building out the "Home" page
+	if selection == "Home":
+		st.title("Lets Do Some Classification")
+		st.markdown("we fight climate change one tweet at a time")
+		header_image = Image.open('resources/6-climatechange.jpg')
+		st.image(header_image, width=500)
+		st.subheader("Fighting Climate Change with AI")
 
 	# Building out the "Information" page
 	if selection == "Information":
@@ -59,6 +131,11 @@ def main():
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+
+	# Building out the "EDA" page
+	if selection == "EDA":
+		st.success("")
+
 
 	# Building out the predication page
 	if selection == "Prediction":
@@ -79,6 +156,8 @@ def main():
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
 
+	
+	
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
 	main()
